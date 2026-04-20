bf7acb5c5f524c1ac812d91b23d0dd694c271c4c
kietduongcit23 <kiet.duong.cit23@eiu.edu.vn>
2026-04-20 14:22:41 +0700

    fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { resolve } from 'node:path';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  base: './',
+  base: '/visual-alg-main/',
   build: {
     rollupOptions: {
       input: {
